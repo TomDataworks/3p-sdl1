fd7a28a34d1c68cb86744abc5682fbcc43a08374
Drake Arconis <drake@alchemyviewer.org>
2014-11-28 18:20:25 -0500

    Add linux64 build config

--- build-cmd.sh
+++ build-cmd.sh
@@ -169,6 +169,80 @@ case "$AUTOBUILD_PLATFORM" in
         popd
     ;;
 
+    "linux64")
+        # Linux build environment at Linden comes pre-polluted with stuff that can
+        # seriously damage 3rd-party builds.  Environmental garbage you can expect
+        # includes:
+        #
+        #    DISTCC_POTENTIAL_HOSTS     arch           root        CXXFLAGS
+        #    DISTCC_LOCATION            top            branch      CC
+        #    DISTCC_HOSTS               build_name     suffix      CXX
+        #    LSDISTCC_ARGS              repo           prefix      CFLAGS
+        #    cxx_version                AUTOBUILD      SIGN        CPPFLAGS
+        #
+        # So, clear out bits that shouldn't affect our configure-directed build
+        # but which do nonetheless.
+        #
+        # unset DISTCC_HOSTS CC CXX CFLAGS CPPFLAGS CXXFLAGS
+
+        # Prefer gcc-4.8 if available.
+        if [[ -x /usr/bin/gcc-4.8 && -x /usr/bin/g++-4.8 ]]; then
+            export CC=/usr/bin/gcc-4.8
+            export CXX=/usr/bin/g++-4.8
+        fi
+
+        # Default target to 64-bit
+        opts="${TARGET_OPTS:--m64}"
+
+        # Handle any deliberate platform targeting
+        if [ -z "$TARGET_CPPFLAGS" ]; then
+            # Remove sysroot contamination from build environment
+            unset CPPFLAGS
+        else
+            # Incorporate special pre-processing flags
+            export CPPFLAGS="$TARGET_CPPFLAGS"
+        fi
+            
+        # Force static linkage to libz by moving .sos out of the way
+        # (Libz is only packaging statics right now but keep this working.)
+        trap restore_sos EXIT
+        for solib in "${stage}"/packages/lib/{debug,release}/libz.so*; do
+            if [ -f "$solib" ]; then
+                mv -f "$solib" "$solib".disable
+            fi
+        done
+
+        pushd "$TOP/$SDL_SOURCE_DIR"
+            # do debug build of sdl
+            PATH="$stage"/bin/:"$PATH" \
+                CFLAGS="-I"$ZLIB_INCLUDE" -I"$PNG_INCLUDE" $opts -Og -g" \
+                CXXFLAGS="-I"$ZLIB_INCLUDE" -I"$PNG_INCLUDE" $opts -Og -g" \
+                CPPFLAGS="-I"$ZLIB_INCLUDE" -I"$PNG_INCLUDE" $opts" \
+                LDFLAGS="-L"$stage/packages/lib/debug" -L"$stage/lib/debug" $opts" \
+                ./configure --target=x86_64-linux-gnu --with-pic \
+                --prefix="$stage" --libdir="$stage/lib/debug" --includedir="$stage/include"
+            make
+            make install
+
+            # clean the build tree
+            make distclean
+
+            # do release build of sdl
+            PATH="$stage"/bin/:"$PATH" \
+                CFLAGS="-I"$ZLIB_INCLUDE" -I"$PNG_INCLUDE" $opts -O3" \
+                CXXFLAGS="-I"$ZLIB_INCLUDE" -I"$PNG_INCLUDE" $opts -O3" \
+                CPPFLAGS="-I"$ZLIB_INCLUDE" -I"$PNG_INCLUDE" $opts" \
+                LDFLAGS="-L"$stage/packages/lib/release" -L"$stage/lib/release" $opts" \
+                ./configure --target=x86_64-linux-gnu --with-pic \
+                --prefix="$stage" --libdir="$stage/lib/release" --includedir="$stage/include"
+            make
+            make install
+
+            # clean the build tree
+            make distclean
+        popd
+    ;;
+
     *)
         exit -1
     ;;
